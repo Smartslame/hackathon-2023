0cd1e660e05680c0edc49964bb10c7c1f599579e
maks <maksimzhu1994@ya.ru>
2023-10-03 04:14:55 +0300

    pagination added

--- src/main/java/sbp/hack/hackdemo/service/InfoService.java
+++ src/main/java/sbp/hack/hackdemo/service/InfoService.java
@@ -14,16 +14,16 @@ import java.util.stream.Collectors;
 public class InfoService {
     private final NodesTableInfoRepository nodesTableInfoRepository;
 
-    public List<NodeInfoDTO> getCitusInfo() {
-        List<NodeTable> nodeInfoCitus = nodesTableInfoRepository.getTableNodeInfoCitus();
+    public List<NodeInfoDTO> getCitusInfo(Integer offset, Integer perPage) {
+        List<NodeTable> nodeInfoCitus = nodesTableInfoRepository.getTableNodeInfoCitus(offset, perPage);
         return nodeInfoCitus.stream().map(x -> new NodeInfoDTO(x.getTableName(),
                         x.getNode(),
                         x.getPrettySize()))
                 .collect(Collectors.toList());
     }
 
-    public List<NodeInfoDTO> getCoordinatorInfo() {
-        List<NodeTable> nodeInfoMaster = nodesTableInfoRepository.getTableNodeInfoMaster();
+    public List<NodeInfoDTO> getCoordinatorInfo(Integer offset, Integer perPage) {
+        List<NodeTable> nodeInfoMaster = nodesTableInfoRepository.getTableNodeInfoMaster(offset, perPage);
         return nodeInfoMaster.stream().map(x -> new NodeInfoDTO(x.getTableName(),
                         x.getNode(),
                         x.getPrettySize()))
